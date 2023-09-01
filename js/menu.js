4cb76425e0260b4a90a78daffbe2bb77b01a7bd5
zhafrans <zhafransiswanto31@gmail.com>
2023-09-01 14:07:49 +0700

    first commit

---
+++ js/menu.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  const showNavbar = (toggleId, navId, bodyId, headerId) => {
+    const toggle = document.getElementById(toggleId),
+      nav = document.getElementById(navId),
+      bodypd = document.getElementById(bodyId),
+      headerpd = document.getElementById(headerId);
+
+    // validasi semua variabel
+    if (toggle && nav && bodypd && headerpd) {
+      toggle.addEventListener("click", () => {
+        // menampilkan navbar
+        nav.classList.toggle("show");
+        //mengubah icons
+        toggle.classList.toggle("bx-x");
+        //padding body
+        bodypd.classList.toggle("body-pd");
+        //padding header
+        headerpd.classList.toggle("body-pd");
+      });
+    }
+  };
+
+  showNavbar("header-toggle", "nav-bar", "body-pd", "header");
+
+  /*===== LINK ACTIVE =====*/
+  const linkColor = document.querySelectorAll(".nav_link");
+
+  function colorLink() {
+    if (linkColor) {
+      linkColor.forEach((l) => l.classList.remove("active"));
+      this.classList.add("active");
+    }
+  }
+  linkColor.forEach((l) => l.addEventListener("click", colorLink));
+});
